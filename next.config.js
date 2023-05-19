82aa9a7cf3e33049c09980c98349d6ea37d3a4d8
cxs2549 <sanchez.cliff.86@gmail.com.com>
2023-05-19 03:21:51 -0700

    address hydration error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/watch',
-            destination: '/',
-            permanent: true,
-          },
-        ];
+  async redirects() {
+    return [
+      {
+        source: "/watch",
+        destination: "/",
+        permanent: true,
       },
+    ]
+  },
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
